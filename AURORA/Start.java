f8034e662bf668f4e5027464faf85a07de07f941
Moynul Mahi <112411790+Mmahi01@users.noreply.github.com>
2024-02-11 11:49:30 +0600

    Update Start.java

--- AURORA/Start.java
+++ AURORA/Start.java
@@ -1,13 +1,9 @@
 import Classes.U_log;
 import javax.swing.*;
 
-
 public class Start {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            U_log uLog = new U_log("default_user");
-            uLog.setVisible(true);
-            });
-        };
+        U_log uLog = new U_log("default_user");
+        uLog.setVisible(true);
     }
-
+}
